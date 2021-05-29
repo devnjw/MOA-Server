4435cf942763460b99e1f2c1e858c6b745f7fbd6
Jinwoo Nam <21600212@handong.edu>
2021-05-29 19:23:38 +0900

    Update participant_model.py

--- app/models/participant_model.py
+++ app/models/participant_model.py
@@ -3,7 +3,7 @@ from app import db
 class Participant(db.Model):
     __tablename__ = 'participant'
     id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True, nullable=False)
-    room_id = db.Column(db.Integer, nullable=False)
+    chat_id = db.Column(db.Integer, nullable=False)
     user_email = db.Column(db.VARCHAR(255), nullable=False)
     enter_time = db.Column(db.TIMESTAMP)
 
@@ -15,7 +15,7 @@ class Participant(db.Model):
 class ParticipantLog(db.Model):
     __tablename__ = 'participant_log'
     id = db.Column(db.Integer, unique=True, primary_key=True, autoincrement=True, nullable=False)
-    room_id = db.Column(db.Integer, nullable=False)
+    chat_id = db.Column(db.Integer, nullable=False)
     user_email = db.Column(db.VARCHAR(255), nullable=False)
     enter_time = db.Column(db.TIMESTAMP)
     exit_time = db.Column(db.TIMESTAMP)
